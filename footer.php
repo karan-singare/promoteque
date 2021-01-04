03997f022437051a470e93c56d5760d13352f95d
Karan Singh Singare <karansingare@gmail.com>
2021-01-05 02:54:03 +0530

    footer added

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+    <?php get_template_part('partials/footer'); ?>
+
     <?php wp_footer(); ?>
   </body>
 </html>
---
+++ partials/footer.php
@@ -0,0 +1,72 @@
+<footer class="footer">
+  <div class="row">
+    <div class="col-1-of-4 policies">
+      <h4>Policies</h4>
+      <ul>
+        <li><a href="">Privacy Policy</a></li>
+        <li><a href="">FAQs</a></li>
+      </ul>
+    </div>
+    <div class="col-1-of-4 offices">
+      <h4>Offices</h4>
+      <ul>
+        <li>New Delhi</li>
+        <li>London</li>
+      </ul>
+    </div>
+    <div class="col-1-of-4 contact">
+      <h4>Contact</h4>
+      <ul>
+        <li>
+          <a href="tel:+44 7584297887">
+            <svg>
+              <use xlink:href="<?php echo get_images_path() . 'sprite.svg#icon-phone'; ?>"></use>
+            </svg>
+            <span>+44 7584297887</span>
+          </a>
+        </li>
+        <li>
+          <a href="mailto:info@thesocialedition.com">
+            <svg>
+              <use xlink:href="<?php echo get_images_path() . 'sprite.svg#icon-mail'; ?>"></use>
+            </svg>
+            <span>info@thesocialedition.com</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-1-of-4 connect">
+      <h4>Connect</h4>
+      <ul>
+        <li>
+          <a href="tel:+44 7584297887">
+            <svg>
+              <use xlink:href="<?php echo get_images_path() . 'sprite.svg#icon-facebook'; ?>"></use>
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a href="tel:+44 7584297887">
+            <svg>
+              <use xlink:href="<?php echo get_images_path() . 'sprite.svg#icon-instagram'; ?>"></use>
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a href="tel:+44 7584297887">
+            <svg>
+              <use xlink:href="<?php echo get_images_path() . 'sprite.svg#icon-twitter'; ?>"></use>
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a href="tel:+44 7584297887">
+            <svg>
+              <use xlink:href="<?php echo get_images_path() . 'sprite.svg#icon-youtube'; ?>"></use>
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</footer>
