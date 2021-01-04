81f1523b80312465e86214a3a205ce3bcc35dbbc
Karan Singh Singare <karansingare@gmail.com>
2021-01-05 01:45:51 +0530

    video is added to the page

---
+++ partials/video.php
@@ -0,0 +1,12 @@
+<div class="video-container">
+  <div class="video">
+    <video class="video" src="<?php echo get_images_path() . 'video.mp4' ?>" autoplay loop mute>
+    </video>
+  </div>
+  <div class="scroll-btn animate__animated animate__bounce animate__infinite infinite">
+    <svg class="">
+      <use xlink:href="<?php echo get_images_path() . 'sprite.svg#icon-chevrons-down' ?>"></use>
+    </svg>
+  </div>
+
+</div>
