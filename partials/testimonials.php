2fd593579969bda031f504c3e546c9a4b11e4a63
Karan Singh Singare <karansingare@gmail.com>
2021-01-05 17:47:44 +0530

    testimonials added

---
+++ partials/testimonials.php
@@ -0,0 +1,49 @@
+<section class="testimonials">>
+  <h2 class="section__heading">What our customers Say</h2>
+  <p class="section__description">
+    Don’t take our word for it, see what our global clients have to say about the work we’ve done for them:
+  </p>
+
+  <div class="row">
+    <div class="">
+      <div class="testimonial">
+        <div class="testimonial__photo">
+          <img src="<?php echo get_images_path() . 'testimonial1.jpg'; ?>" alt="" class="testimonial__img">
+        </div>
+        <div class="testimonial__content">
+          <p class="testimonial__text">
+            Thank you so much Teacup Digital for the fantastic work on the new website and social media presence. We can see the huge difference this is going to make to our business. The team’s expertise and attention to detail has not gone unnoticed, and your effort is much appreciated.
+          </p>
+          <h3 class="testimonial__author">The Kaftan Company</h3>
+          <h4 class="testimonial__designation">Account Manager</h4>
+        </div>
+      </div>
+      <div class="testimonial">
+        <div class="testimonial__photo">
+          <img src="<?php echo get_images_path() . 'testimonial2.jpg'; ?>" alt="" class="testimonial__img">
+        </div>
+        <div class="testimonial__content">
+          <p class="testimonial__text">
+            Thank you so much Teacup Digital for the fantastic work on the new website and social media presence. We can see the huge difference this is going to make to our business. The team’s expertise and attention to detail has not gone unnoticed, and your effort is much appreciated.
+          </p>
+          <h3 class="testimonial__author">The Kaftan Company</h3>
+          <h4 class="testimonial__designation">Account Manager</h4>
+        </div>
+      </div>
+      <div class="testimonial">
+        <div class="testimonial__photo">
+          <img src="<?php echo get_images_path() . 'testimonial3.jpg'; ?>" alt="" class="testimonial__img">
+        </div>
+        <div class="testimonial__content">
+          <p class="testimonial__text">
+            Thank you so much Teacup Digital for the fantastic work on the new website and social media presence. We can see the huge difference this is going to make to our business. The team’s expertise and attention to detail has not gone unnoticed, and your effort is much appreciated.
+          </p>
+          <h3 class="testimonial__author">The Kaftan Company</h3>
+          <h4 class="testimonial__designation">Account Manager</h4>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</section>
