baf8f423c35f21dbb554494a3d95f80989869fbf
Karan Singh Singare <karansingare@gmail.com>
2021-01-05 19:57:27 +0530

    about-us page created

--- templates/page-about-us.php
+++ templates/page-about-us.php
@@ -8,7 +8,7 @@
   load_banner('about-us');
 
  ?>
-<section class="about-us-info">
+<section class="about-us">
   <div class="row">
     <p>
       The Social Edition provides all the design, content, PR and social media optimisation brands need to ensure their campaign has maximum impact with audiences that matter, on channels that matter. We help uncover unique opportunities for
@@ -16,7 +16,7 @@
       solutions through to social media management and influencer-led content amplification.
     </p>
 
-    <h3 class="heading__secondary">Who are we?</h3>
+    <h3 class="section__heading">Who are we?</h3>
 
     <p>
       The Social Edition is powerhouse of talent and expertise, led by a team of experts with years of global experience in the fields of digital marketing, brand management, design & consultancy. We are insiders in a highly competitive and complex
@@ -28,31 +28,28 @@
 
 </section>
 
-<section class="administration">
+<div class="administration">
 
-  <div class="photo-composition photo-composition--1">
-    <div class="row custom-row">
-      <div class="photo-composition__content">
-        <div class="photo-composition__content--left">
-          <h2 class="photo-composition__title">
-            Priyanka Mehra Dayal <br>
-            <span>Founder</span>
-          </h2>
-        </div>
-
-        <div class="photo-composition__content--right">
-          <p class="photo-composition__text">
-            Priyanka is the Founder of The Social Edition and CEO of influencer marketing platform, Teacup Influence. She brings over 20 years of experience in brand marketing and specialist consultancy. After graduating from Michigan, USA, she
-            started her career as a journalist in New York, covering advertising and sports sponsorships. With a strong knowledge and on-ground experience on the business of brand marketing, Priyanka went on to work for a leading influencer marketing
-            agency in London. For the past 9 years, Priyanka has helped global fashion, lifestyle and luxury brands identify and develop data-driven influencer & brand partnership opportunities in order to amplify and maximise ROI within overarching
-            digital marketing strategies. Priyanka specialises in helping businesses navigate the rapidly evolving the global digital marketing landscape, whilst staying on top of the game and ahead of the trends.
-          </p>
-        </div>
+  <div class="photo-composition photo-composition--updown">
+    <div class="photo-composition__content">
+      <div class="photo-composition__content--left">
+        <p class="photo-composition__text">
+          Priyanka is the Founder of The Social Edition and CEO of influencer marketing platform, Teacup Influence. She brings over 20 years of experience in brand marketing and specialist consultancy. After graduating from Michigan, USA, she
+          started her career as a journalist in New York, covering advertising and sports sponsorships. With a strong knowledge and on-ground experience on the business of brand marketing, Priyanka went on to work for a leading influencer marketing
+          agency in London. For the past 9 years, Priyanka has helped global fashion, lifestyle and luxury brands identify and develop data-driven influencer & brand partnership opportunities in order to amplify and maximise ROI within overarching
+          digital marketing strategies. Priyanka specialises in helping businesses navigate the rapidly evolving the global digital marketing landscape, whilst staying on top of the game and ahead of the trends.
+        </p>
       </div>
+      <div class="photo-composition__content--right">
+        <h3 class="photo-composition__title">
+          Priyanka Mehra Dayal
+        </h3>
+        <span>Founder</span>
+      </div>
+
     </div>
   </div>
 
-
-</section>
+</div>
 
 <?php get_footer(); ?>
