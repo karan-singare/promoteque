cef6678599771f54e83921fa8349a5e8ff8d56ea
Karan Singh Singare <karansingare@gmail.com>
2021-01-12 18:03:29 +0530

    photo-composition modified

--- partials/photo-composition.php
+++ partials/photo-composition.php
@@ -1,44 +1,22 @@
-<div>
-  <div class="photo-composition photo-composition--1">
-    <div class="photo-composition__content">
-      <div class="photo-composition__content--left">
-        <h3 class="photo-composition__heading">
-          Creation + Strategy + Amplification
-        </h3>
-        <p class="photo-composition__text">
-          From strategy and planning right through to website creation, design innovation, social media and content amplification, we’ve got it covered.
-        </p>
-      </div>
-      <div class="photo-composition__content--right">
-        <h3 class="photo-composition__title">
-          We take a holistic approach to branding and promotion by taking on each brand from inception to digital transformation.
-        </h3>
-        <a href="" class="btn btn--hollow">Learn More</a>
-      </div>
-
-    </div>
+<div class="photo-composition">
+  <div class="photo-composition__photo">
+    <img src="<?php echo get_images_path() . 'skateboard.jpg'; ?>" alt="">
+    <h2>Creation + Strategy + Amplification</h2>
   </div>
-
-  <div class="photo-composition photo-composition--2">
-    <div class="photo-composition__content">
-
-      <div class="photo-composition__content--right">
-        <h3 class="photo-composition__title">
-          We’re experts in engaging audiences that matter, and converting clicks to conversations.
-        </h3>
-        <a href="" class="btn btn--hollow">Learn More</a>
-      </div>
-
-      <div class="photo-composition__content--left">
-        <h3 class="photo-composition__heading">
-          Global approach + Local appeal
-        </h3>
-        <p class="photo-composition__text">
-          Whether you’re a start-up looking for a place to build it all, or an existing business looking for a refresh, our team of content, design and social media specialists provide world-class marketing solutions customised to suit your needs.
-        </p>
-      </div>
-
-    </div>
+  <div class="photo-composition__content">
+    <h3>We take a holistic approach to branding and promotion by taking on each brand from inception to digital transformation.</h3>
+    <p>From strategy and planning right through to website creation, design innovation, social media and content amplification, we’ve got it covered.</p>
+    <a href="#" class="btn btn--hollow">learn more</a>
   </div>
-
+</div>
+<div class="photo-composition">
+  <div class="photo-composition__content">
+    <h3>We’re experts in engaging audiences that matter, and converting clicks to conversations.</h3>
+    <p>Whether you’re a start-up looking for a place to build it all, or an existing business looking for a refresh, our team of content, design and social media specialists provide world-class marketing solutions customised to suit your needs</p>
+    <a href="#" class="btn btn--hollow">learn more</a>
+  </div>
+  <div class="photo-composition__photo">
+    <img src="<?php echo get_images_path() . 'biking.jpg'; ?>" alt="">
+    <h2>Global approach + Local appeal</h2>
+  </div>
 </div>
--- partials/strength.php
+++ partials/strength.php
@@ -1,36 +1,9 @@
 <section class="strength">
   <div class="row">
-    <div class="icons-composition">
-      <div class="icons-grid icons-grid--left">
-        <div class="icons-grid-item">
-          <svg>
-            <use xlink:href="<?php echo get_images_path() . "sprite.svg#icon-stats-bars" ?>"></use>
-          </svg>
-          <p>Driven by Insights & Trends</p>
-        </div>
-        <div class="icons-grid-item">
-          <svg>
-            <use xlink:href="<?php echo get_images_path() . "sprite.svg#icon-color-palette" ?>"></use>
-          </svg>
-          <p>Built on Creativity & Originality</p>
-        </div>
-        <div class="icons-grid-item">
-          <svg>
-            <use xlink:href="<?php echo get_images_path() . "sprite.svg#icon-power" ?>"></use>
-          </svg>
-          <p>Powered by Ideas & Innovation</p>
-        </div>
-      </div>
-
-      <div class="icons-composition__content">
-        <h1 class="heading__primary heading__primary--main">Our core strengths</h1>
-        <p class="icons-composition__details">
-          We listen…and then we get right to it. In this fast-paced world of digital media, we know what it takes for brands to break through the clutter and create a strong and truly meaningful presence online.
-        </p>
-        <a href="" class="btn btn--hollow">Learn More</a>
-      </div>
-
-    </div>
+    <h1>Our core strengths</h1>
+    <p>
+      We listen…and then we get right to it. In this fast-paced world of digital media, we know what it takes for brands to break through the clutter and create a strong and truly meaningful presence online.
+    </p>
 
   </div>
 </section>
