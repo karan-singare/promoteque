c6c0679ebde2c36c5b9960baa082e15a38ce0f9f
Karan Singh Singare <karansingare@gmail.com>
2021-01-13 20:38:10 +0530

    video frame is added inside the gallery

--- partials/testimonials.php
+++ partials/testimonials.php
@@ -8,38 +8,41 @@
     <div class="owl-carousel owl-theme">
       <div class="testimonial">
         <div class="testimonial__photo">
-          <img src="<?php echo get_images_path() . 'testimonial1.jpg'; ?>" alt="" class="testimonial__img" loading="lazy">
+          <img src="<?php echo get_images_path() . 'testimonial1.jpeg'; ?>" alt="" class="testimonial__img" loading="lazy">
         </div>
         <div class="testimonial__content">
           <p class="testimonial__text">
-            Thank you so much Teacup Digital for the fantastic work on the new website and social media presence. We can see the huge difference this is going to make to our business. The team’s expertise and attention to detail has not gone unnoticed, and your effort is much appreciated.
+            "Thank you for all the effort that you put in to make the launch of the brand a success. The social media was handled beautifully.”
           </p>
-          <h3 class="testimonial__author">The Kaftan Company</h3>
-          <h4 class="testimonial__designation">Account Manager</h4>
+          <h3 class="testimonial__author">Namrata Rajpal</h3>
+          <h3 class="testimonial__compay">The House of Exotique</h3>
+          <h4 class="testimonial__designation">Founder and Creative Director</h4>
         </div>
       </div>
       <div class="testimonial">
         <div class="testimonial__photo">
-          <img src="<?php echo get_images_path() . 'testimonial2.jpg'; ?>" alt="" class="testimonial__img" loading="lazy">
+          <img src="<?php echo get_images_path() . 'testimonial2.jpeg'; ?>" alt="" class="testimonial__img" loading="lazy">
         </div>
         <div class="testimonial__content">
           <p class="testimonial__text">
-            Thank you so much Teacup Digital for the fantastic work on the new website and social media presence. We can see the huge difference this is going to make to our business. The team’s expertise and attention to detail has not gone unnoticed, and your effort is much appreciated.
+            “The team at The Social Edition have been excellent. They have helped me startegise, plan and execute a winning personal branding presence across multiple platforms including help launch my Podcast from scratch.”
           </p>
-          <h3 class="testimonial__author">The Kaftan Company</h3>
-          <h4 class="testimonial__designation">Account Manager</h4>
+          <h3 class="testimonial__author">Meenakshi Kuwadekar</h3>
+          <h3 class="testimonial__compay"></h3>
+          <h4 class="testimonial__designation">Luxury & Lifestyle Consultant and Podcaster</h4>
         </div>
       </div>
       <div class="testimonial">
         <div class="testimonial__photo">
-          <img src="<?php echo get_images_path() . 'testimonial3.jpg'; ?>" alt="" class="testimonial__img" loading="lazy">
+          <img src="<?php echo get_images_path() . 'testimonial3.jpeg'; ?>" alt="" class="testimonial__img" loading="lazy">
         </div>
         <div class="testimonial__content">
           <p class="testimonial__text">
-            Thank you so much Teacup Digital for the fantastic work on the new website and social media presence. We can see the huge difference this is going to make to our business. The team’s expertise and attention to detail has not gone unnoticed, and your effort is much appreciated.
+          “What really differentiates The Social Edition is the superiority of their content strategy. Priyanka and the team bring a well planned, result-driven offering to the table.”
           </p>
-          <h3 class="testimonial__author">The Kaftan Company</h3>
-          <h4 class="testimonial__designation">Account Manager</h4>
+          <h3 class="testimonial__author">Krushmi Chheda</h3>
+          <h3 class="testimonial__compay">CORE</h3>
+          <h4 class="testimonial__designation">Sports Scientist and Nutritionist, Founder of CORE</h4>
         </div>
       </div>
     </div>
--- partials/work.php
+++ partials/work.php
@@ -12,25 +12,42 @@
 
   <div class="row">
     <div class="gallery">
-      <?php foreach ($images as $image): ?>
+
+      <div class="gallery__video-item gallery__video-item--1">
+        <video src="<?php echo get_images_path() . 'gallery-vid1.mp4'; ?>" paused muted loop>
+
+        </video>
+      </div>
+      <div class="gallery__image-items">
+        <!-- <img src="<?php echo $images[0]; ?>" alt=""> -->
+      </div>
+      <div class="gallery__video-item gallery__video-item--2">
+        <video src="<?php echo get_images_path() . 'gallery-vid1.mp4'; ?>" paused muted loop>
+
+        </video>
+        <img src="<?php echo get_images_path() . 'iphone.png'; ?>" alt="" class="gallery__img" loading="lazy">
+      </div>
+
+
+      <!-- <?php foreach ($images as $image): ?>
 
         <div class="gallery__item">
           <img src="<?php echo $image; ?>" alt="" class="gallery__img" loading="lazy">
         </div>
 
-      <?php endforeach; ?>
+      <?php endforeach; ?> -->
 
-      <div class="gallery__item gallery__item--video">
-        <video src="<?php echo get_images_path() . 'gallery-vid.mp4'; ?>" autoplay muted loop>
+      <!-- <div class="gallery__item gallery__item--video">
+        <video src="<?php echo get_images_path() . 'gallery-vid.mp4'; ?>" paused muted loop>
 
         </video>
-      </div>
-      <div class="gallery__item gallery__item--iphone">
+      </div> -->
+      <!-- <div class="gallery__item gallery__item--iphone">
         <img src="<?php echo get_images_path() . 'iphone.png'; ?>" alt="" class="gallery__img" loading="lazy">
-        <video src="<?php echo get_images_path() . 'gallery-vid.mp4'; ?>" autoplay muted loop>
+        <video src="<?php echo get_images_path() . 'gallery-vid2.mp4'; ?>" paused muted loop>
 
         </video>
-      </div>
+      </div> -->
     </div>
   </div>
 
