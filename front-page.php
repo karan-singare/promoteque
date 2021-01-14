b70b37636ea78b22f6eaa53f9f39278e2e94bb0a
Karan Singh Singare <karansingare@gmail.com>
2021-01-15 04:09:27 +0530

    work section with the video

--- front-page.php
+++ front-page.php
@@ -2,11 +2,11 @@
 
 
   <?php get_template_part('partials/video'); ?>
-  <?php //get_template_part('partials/story'); ?>
+  <?php get_template_part('partials/story'); ?>
   <?php get_template_part('partials/photo-composition'); ?>
-  <?php // get_template_part('partials/strength'); ?>
+  <?php get_template_part('partials/strength'); ?>
   <?php // get_template_part('partials/clients'); ?>
-  <?php // get_template_part('partials/work'); ?>
+  <?php get_template_part('partials/work'); ?>
   <?php get_template_part('partials/testimonials'); ?>
 
 <?php get_footer(); ?>
