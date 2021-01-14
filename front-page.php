d880f55c85bdf447d62d2391a9c0bf41bea31714
Karan Singh Singare <karansingare@gmail.com>
2021-01-15 03:02:53 +0530

    photo-composition fixed

--- front-page.php
+++ front-page.php
@@ -2,8 +2,8 @@
 
 
   <?php get_template_part('partials/video'); ?>
-  <?php // get_template_part('partials/story'); ?>
-  <?php // get_template_part('partials/photo-composition'); ?>
+  <?php //get_template_part('partials/story'); ?>
+  <?php get_template_part('partials/photo-composition'); ?>
   <?php // get_template_part('partials/strength'); ?>
   <?php // get_template_part('partials/clients'); ?>
   <?php // get_template_part('partials/work'); ?>
