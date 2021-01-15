3b5a97321128a131910889fd7043b471ff695f9d
Karan Singh Singare <karansingare@gmail.com>
2021-01-15 06:24:17 +0530

    services fixed

--- templates/page-services.php
+++ templates/page-services.php
@@ -18,53 +18,55 @@
 
  ?>
 <section class="services-section">
-  <div class="services">
-    <?php if ( $services->have_posts() ): ?>
+  <div class="row">
+    <div class="services">
+      <?php if ( $services->have_posts() ): ?>
 
 
-      <?php while( $services->have_posts() ): $services->the_post(); ?>
-        <?php if ($i < 4): ?>
-          <div class="service service__light">
-            <span class="service__number"><?php echo "0". ++$i . ". "; ?></span>
-            <h3 class="service__title"><?php the_title(); ?></h3>
-            <div class="service__description">
-              <?php the_excerpt(); ?>
+        <?php while( $services->have_posts() ): $services->the_post(); ?>
+          <?php if ($i < 4): ?>
+            <div class="service service__light">
+              <span class="service__number"><?php echo "0". ++$i . ". "; ?></span>
+              <h3 class="service__title"><?php the_title(); ?></h3>
+              <div class="service__description">
+                <?php the_excerpt(); ?>
+              </div>
+              <div class="service__details">
+                <?php the_content(); ?>
+              </div>
+
+              <div class="service__separator"></div>
+
             </div>
-            <div class="service__details">
-              <?php the_content(); ?>
+          <?php else: ?>
+
+            <div class="service service__dark">
+              <span class="service__number"><?php echo "0". ++$i . ". "; ?></span>
+              <h3 class="service__title"><?php the_title(); ?></h3>
+              <div class="service__description">
+                <?php the_excerpt(); ?>
+              </div>
+              <div class="service__details">
+                <?php the_content(); ?>
+              </div>
+
+              <div class="service__separator"></div>
+
             </div>
 
-            <div class="service__separator"></div>
 
-          </div>
-        <?php else: ?>
-
-          <div class="service service__dark">
-            <span class="service__number"><?php echo "0". ++$i . ". "; ?></span>
-            <h3 class="service__title"><?php the_title(); ?></h3>
-            <div class="service__description">
-              <?php the_excerpt(); ?>
-            </div>
-            <div class="service__details">
-              <?php the_content(); ?>
-            </div>
-
-            <div class="service__separator"></div>
-
-          </div>
-
-
-        <?php endif; ?>
+          <?php endif; ?>
 
 
 
-      <?php endwhile; ?>
-    <?php endif; ?>
+        <?php endwhile; ?>
+      <?php endif; ?>
 
-    <?php wp_reset_postdata();  ?>
+      <?php wp_reset_postdata();  ?>
+    </div>
   </div>
 
-  .<a href="#" class="service--btn btn btn--hollow">Get in touch</a>
+  <!-- <a href="#" class="service--btn btn btn--hollow">Get in touch</a> -->
 
 </section>
 
